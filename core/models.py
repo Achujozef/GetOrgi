7d628282226335f7c6ecc207e1670bb1387cc521
achujoseph0709 <achujozefsl0709@gmail.com>
2025-04-27 14:01:14 +0530

    Card Rezized and migrations changed

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField()
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     rating = models.FloatField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
     purchase_count = models.PositiveIntegerField(default=0)
